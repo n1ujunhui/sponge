5082322db6a8c0b9cd1f2ff58a43273bfeea8e36
Daniel Zhu <niujunhui@sjtu.edu.cn>
2022-04-04 19:07:08 +0800

    lab1.

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -1,4 +1,5 @@
 #include "stream_reassembler.hh"
+#include <cstring>
 
 // Dummy implementation of a stream reassembler.
 
@@ -12,15 +13,125 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+#define BUFFER_STREAM_OFFSET(index) buffer_for_offset[index]
 
+StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {
+    this->buffer = std::make_shared<Buffer>(capacity);
+    this->stream_need_offset = 0;
+    this->_eof = false;
+}
+
+inline std::tuple<size_t, size_t, bool, size_t> StreamReassembler::whetherToBuffer(const size_t index, const size_t size) {
+    // clear useless buffers first
+    size_t buffer_needed_offset = buffer->getValidStartOffset();
+    if(index + size < buffer_needed_offset) return {0, 0, false, 0};
+    else if (index > buffer_needed_offset) {
+        if(size <= _capacity - (index - buffer_needed_offset))
+            return {index, 0, true, size};
+        else return {index, 0, true, _capacity - (index - buffer_needed_offset)};
+    } else {
+        if(size - (buffer_needed_offset - index) <= _capacity)
+            return {buffer_needed_offset, buffer_needed_offset - index, true, index + size - buffer_needed_offset};
+        else return {buffer_needed_offset, buffer_needed_offset - index, true, _capacity};
+    }
+    return {0,0, false, 0};
+    
+    // size_t buffer_index = buffer_start_index;
+    // while(BUFFER_STREAM_OFFSET(buffer_index) < stream_need_offset && (BUFFER_STREAM_OFFSET(buffer_index)!=0)) {
+    //     buffer_for_offset[buffer_index] = 0;
+    //     buffer_used--;
+    //     buffer_left_capacity++;
+    //     buffer_index++;
+    //     _unassembled_bytes--;
+    //     if(buffer_index == _capacity) buffer_index = 0;
+    // }
+
+    // // write some buffer into the stream
+    // if(BUFFER_STREAM_OFFSET(buffer_index) == stream_need_offset) {
+    //     size_t remaining_capacity = _output.remaining_capacity();
+    //     size_t in_stream_size = 0;
+    //     for(size_t i = 0; i < remaining_capacity; ++i) {
+    //         if(buffer_stream_offset(buffer_index+i)!=0) {
+    //             in_stream_size++;
+    //         } else {
+    //             break;
+    //         }
+    //     }
+    //     if(buffer_index + in_stream_size >= _capacity) {
+    //         _output.write(std::string(&buffer_for_char[buffer_index], _capacity - buffer_index));
+    //         _output.write(std::string(&buffer_for_char[0], buffer_index + in_stream_size - _capacity));
+    //         for(size_t i = buffer_index; i < _capacity; ++i) {
+    //             buffer_stream_offset(i) = 0;
+    //         }
+    //         for(size_t i = 0; i < buffer_index + in_stream_size - _capacity; ++i) {
+    //             buffer_stream_offset(i) = 0;
+    //         }
+    //         buffer_start_index = buffer_index + in_stream_size - _capacity;
+    //     } else {
+    //         _output.write(std::string(&buffer_for_char[buffer_index], in_stream_size));
+    //         for(size_t i = 0; i < in_stream_size; ++i) {
+    //             buffer_stream_offset(buffer_index + i) = 0;
+    //         }
+    //         buffer_start_index = buffer_index + in_stream_size;
+    //     }
+    //     buffer_used-=in_stream_size;
+    //     buffer_left_capacity+=in_stream_size;
+    //     stream_need_offset+=in_stream_size;
+    //     _unassembled_bytes-=in_stream_size;
+    // }
+
+    // if(index <= stream_need_offset) return false;
+
+    // if(buffer_for_offset[buffer_start_index + index - stream_need_offset]!=0) return false;
+
+    // if(index - stream_need_offset + size > buffer_left_capacity) return false;
+
+    // return true;
+}
+
+inline std::pair<size_t, bool> StreamReassembler::whetherToStream(const size_t index, const size_t size) {
+    if(index <= stream_need_offset) {
+        if(index+size<stream_need_offset) return std::make_pair(0, false);
+        else return std::make_pair(stream_need_offset-index, true);
+    } else return std::make_pair(0, false);
+}
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    size_t data_size = data.length();
+    printf("push string: %s\n", data.c_str());
+    auto _whether2Stream = whetherToStream(index, data_size);
+    if(_whether2Stream.second) {
+        auto str_index = _whether2Stream.first;
+        auto writed_num = _output.write(data.substr(str_index));
+        printf("write from string index: %ld, writing %ld bytes.\n", str_index, writed_num);
+        printf("data size: %ld\n", data_size);
+        stream_need_offset += (writed_num);
+        if(str_index + writed_num == data_size) {
+            stream_need_offset = buffer->removeUselessBuffer(stream_need_offset, _output);
+            printf("needing: %ld\n", stream_need_offset);
+            printf("unused buffer: %ld\n", buffer->remainingStorage());
+            if(eof) {
+                this->_output.end_input();
+            }
+        } else {
+            auto data_left = data.substr(str_index+writed_num);
+            auto _whether2Buffer = whetherToBuffer(index+str_index+writed_num, data_size-str_index+writed_num);
+            printf("whether buffer: %d\n", std::get<2>(_whether2Buffer));
+            if(std::get<2>(_whether2Buffer)) {
+                buffer->enBuffer(std::get<0>(_whether2Buffer), data_left.substr(std::get<1>(_whether2Buffer), std::get<3>(_whether2Buffer)), eof);
+            }
+        }
+    } else {
+        auto _whether2Buffer = whetherToBuffer(index, data_size);
+        printf("whether buffer: %d\n", std::get<2>(_whether2Buffer));
+        if(std::get<2>(_whether2Buffer)) {
+            buffer->enBuffer(std::get<0>(_whether2Buffer), data.substr(std::get<1>(_whether2Buffer), std::get<3>(_whether2Buffer)), eof);
+        }
+    }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const { return buffer->getUnassembled(); }
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { return buffer->getUnassembled() == 0; }
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -15,6 +15,154 @@ class StreamReassembler {
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
 
+    class Buffer {
+      private:
+        std::unique_ptr<char[]> buffer_for_char{};
+        std::unique_ptr<bool[]> valid_flags{};
+        size_t valid_start_offset{};
+        size_t valid_index{};
+        size_t used_storage{};
+        size_t _capacity{};
+        size_t eof_index{};
+        bool eof_set{};
+
+        inline size_t indexToOffset(size_t index) {
+          if (index < valid_index) {
+            return index + _capacity - valid_index + valid_start_offset;
+          } else return index - valid_index + valid_start_offset;
+        }
+
+        inline char& indexToChar(size_t index) {
+          return buffer_for_char[index];
+        }
+
+        inline void singleWrite(size_t index, const char & c, bool eof) {
+          // printf("writing %c, used_storage = %ld\n", c, used_storage);
+          while(index>=_capacity) {
+            index-=_capacity;
+          }
+          if(valid_flags[index] == true) return;
+          if(eof) {
+            this->eof_set = true;
+            this->eof_index = index;
+          }
+          buffer_for_char[index] = c;
+          valid_flags[index] = true;
+          used_storage++;
+        }
+      
+      public:
+        Buffer(size_t capacity) {
+          this->buffer_for_char = std::unique_ptr<char[]>(new char[capacity]);
+          this->valid_flags = std::unique_ptr<bool[]>(new bool[capacity]);
+          this->valid_index = 0;
+          this->used_storage = 0;
+          this->valid_start_offset = 0;
+          this->_capacity = capacity;
+          for(size_t i = 0; i < capacity; ++i) {
+            valid_flags[i] = false;
+          }
+          this->eof_set = false;
+          this->eof_index = 0;
+        }
+
+        size_t getValidStartOffset() {
+          return valid_start_offset;
+        }
+
+        void enBuffer(size_t string_offset, const std::string &data, bool eof) {
+          size_t data_size = data.length();
+          size_t buffer_start_offset = indexToOffset(valid_index);
+          size_t start_write_index = valid_index + string_offset - buffer_start_offset;
+          for(size_t i = 0; i < data_size; ++i) {
+            singleWrite(start_write_index+i, data.at(i), eof);
+          }
+        }
+
+        size_t remainingStorage() {
+          return _capacity - used_storage;
+        }
+
+        size_t getUnassembled() {
+          return used_storage;
+        }
+
+        size_t removeUselessBuffer(size_t needed_offset, ByteStream &stream) {
+          // printf("need: %ld\n", needed_offset);
+          // printf("now valid_start_offset: %ld\n", valid_start_offset);
+          // printf("now used storage: %ld\n", used_storage);
+          while(needed_offset > valid_start_offset) {
+            if(valid_flags[valid_index]) {
+              valid_flags[valid_index] = false;
+              used_storage--;
+            }
+            valid_start_offset++;
+            valid_index++;
+            if(valid_index == _capacity) valid_index = 0;
+          }
+          // printf("now valid_start_offset: %ld\n", valid_start_offset);
+          // printf("now used storage: %ld\n", used_storage);
+          size_t writing_size = 0;
+          size_t tmp_index = valid_index;
+          bool overflow = false;
+          bool _end = false;
+          size_t stream_remaining_capacity = stream.remaining_capacity();
+          // printf("stream has %ld unused.\n", stream_remaining_capacity);
+          while(writing_size<stream_remaining_capacity && valid_flags[tmp_index]) {
+            if(eof_set && eof_index == tmp_index) _end = true;
+            writing_size ++;
+            tmp_index++;
+            if(tmp_index == _capacity) {
+              overflow = true;
+              tmp_index = 0;
+            }
+          }
+          // printf("writing %ld bytes.\n", writing_size);
+          if(writing_size > 0) {
+            if(!overflow) {
+              auto start = &valid_flags[valid_index];
+              auto end = start + writing_size;
+              std::fill(start, end, false);
+              stream.write(std::string(&buffer_for_char[valid_index], writing_size));
+              valid_index += writing_size;
+              used_storage -= writing_size;
+              if(valid_index == _capacity) valid_index = 0;
+              valid_start_offset += writing_size;
+            } else {
+              auto start = &valid_flags[valid_index];
+              auto end = start + _capacity-valid_index;
+              std::fill(start, end, false);
+              start = &valid_flags[0];
+              end = start + writing_size + valid_index - _capacity;
+              std::fill(start, end, false);
+              stream.write(std::string(&buffer_for_char[valid_index], _capacity-valid_index));
+              stream.write(std::string(&buffer_for_char[0], writing_size + valid_index - _capacity));
+              valid_index = writing_size + valid_index - _capacity;
+              used_storage -= writing_size;
+              valid_start_offset += writing_size;
+            }
+            if(_end) stream.end_input();
+          }
+           // printf("now used storage: %ld\n", used_storage);
+          return valid_start_offset;
+        }
+
+    };
+
+    std::shared_ptr<Buffer> buffer{};
+
+    inline std::tuple<size_t, size_t, bool, size_t> whetherToBuffer(const size_t index, const size_t size);
+
+    inline std::pair<size_t, bool> whetherToStream(const size_t index, const size_t size);
+
+    inline size_t & buffer_stream_offset(size_t);
+
+    void insert2Buffer(const std::string &) const;
+
+    size_t stream_need_offset{};
+    
+    bool _eof{};
+
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
     //! \note This capacity limits both the bytes that have been reassembled,
